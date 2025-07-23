ec7fd0ee4b4f89a9c4f59165cb5ba0e7102ccc65
maksym <vasyl.lanko@tecnico.ulisboa.pt>
2025-07-23 14:40:38 +0200

    completed t_uts nested test + added require.user root

--- tests/kernel/t_uts.c
+++ tests/kernel/t_uts.c
@@ -14,6 +14,9 @@ ATF_TC(basic_uts_namespace);
 ATF_TC_HEAD(basic_uts_namespace, tc)
 {
     atf_tc_set_md_var(tc, "descr", "Test unshare system call for uts namespace");
+    // TODO:  Please do not use this unless absolutely necessary!
+    // You can most likely make your tests run as a regular user if you use puffs and rump.
+    atf_tc_set_md_var(tc, "require.user", "root");
 }
 ATF_TC_BODY(basic_uts_namespace, tc)
 {
@@ -28,7 +31,7 @@ ATF_TC_BODY(basic_uts_namespace, tc)
         char modified_hostname[256];
 
         ATF_REQUIRE(syscall(507, CLONE_NEWUTS) == 0);
-       
+
         ATF_REQUIRE(sethostname("new_hostname", strlen("new_hostname")) == 0);
         ATF_REQUIRE(gethostname(modified_hostname, sizeof(modified_hostname)) == 0);
         ATF_CHECK(strcmp(modified_hostname, "new_hostname") == 0);
@@ -56,12 +59,70 @@ ATF_TC(nested_uts_namespace);
 ATF_TC_HEAD(nested_uts_namespace, tc)
 {
     atf_tc_set_md_var(tc, "descr", "Test nested unshare system call for uts namespace");
+    // TODO:  Please do not use this unless absolutely necessary!
+    // You can most likely make your tests run as a regular user if you use puffs and rump.
+    atf_tc_set_md_var(tc, "require.user", "root");
 }
 ATF_TC_BODY(nested_uts_namespace, tc)
 {
-    // TODO: nested
+    pid_t pid1, pid2;
     char original_hostname[256];
     ATF_REQUIRE(gethostname(original_hostname, sizeof(original_hostname)) == 0);
+
+    pid1 = fork();
+    if (pid1 == 0) {
+        char modified_hostname[256];
+
+        ATF_REQUIRE(syscall(507, CLONE_NEWUTS) == 0);
+
+        ATF_REQUIRE(sethostname("new_hostname", strlen("new_hostname")) == 0);
+        ATF_REQUIRE(gethostname(modified_hostname, sizeof(modified_hostname)) == 0);
+        ATF_CHECK(strcmp(modified_hostname, "new_hostname") == 0);
+        printf("modified: %s\n", modified_hostname);
+
+        pid2 = fork();
+        if (pid2 == 0) {
+            char second_hostname[256];
+
+            ATF_REQUIRE(syscall(507, CLONE_NEWUTS) == 0);
+
+            ATF_REQUIRE(sethostname("second_hostname", strlen("second_hostname")) == 0);
+            ATF_REQUIRE(gethostname(second_hostname, sizeof(second_hostname)) == 0);
+            ATF_CHECK(strcmp(second_hostname, "second_hostname") == 0);
+            printf("second: %s\n", modified_hostname);
+
+            exit(0);
+
+        } else {
+            char middle_hostname[256];
+            int status2;
+            waitpid(pid2, &status2, 0);
+            // needed to fail test if child doesn't know unshare syscall
+            if (!WIFEXITED(status2) || WEXITSTATUS(status2) != 0) {
+                atf_tc_fail("Nested child process failed");
+            }
+            ATF_REQUIRE(gethostname(middle_hostname, sizeof(middle_hostname)) == 0);
+            ATF_CHECK(strcmp(middle_hostname, modified_hostname) == 0);
+            printf("middle: %s\n", middle_hostname);
+
+            exit(0);
+        }
+
+    } else {
+        char parent_hostname[256];
+        int status1;
+        waitpid(pid1, &status1, 0);
+        // needed to fail test if child doesn't know unshare syscall
+        if (!WIFEXITED(status1) || WEXITSTATUS(status1) != 0) {
+            atf_tc_fail("Child process failed");
+        }
+
+        ATF_REQUIRE(gethostname(parent_hostname, sizeof(parent_hostname)) == 0);
+        ATF_CHECK(strcmp(parent_hostname, original_hostname) == 0);
+        printf("parent: %s\n", parent_hostname);
+
+    }
+
 }
 
 ATF_TP_ADD_TCS(tp)
