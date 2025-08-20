5fd50cf50e52b6c3313ba59a2fb2d8895aed8749
maksym <vasyl.lanko@tecnico.ulisboa.pt>
2025-08-20 16:42:55 +0200

    remove double include in kern_sysctl.c

--- sys/kern/kern_sysctl.c
+++ sys/kern/kern_sysctl.c
@@ -74,9 +74,8 @@ __KERNEL_RCSID(0, "$NetBSD: kern_sysctl.c,v 1.271 2024/09/08 09:36:51 rillig Exp
 
 #ifdef _KERNEL_OPT
 #include "opt_defcorename.h"
-#if defined(NAMESPACES) && defined(NS_UTS)
-#include <sys/uts.h>
-#endif
+#include "opt_ns.h"
+#include "opt_ns_uts.h"
 #endif
 
 #include "ksyms.h"
@@ -98,11 +97,6 @@ __KERNEL_RCSID(0, "$NetBSD: kern_sysctl.c,v 1.271 2024/09/08 09:36:51 rillig Exp
 #include <sys/systm.h>
 
 #include <crypto/blake2/blake2s.h>
-
-#ifdef _KERNEL_OPT
-#include "opt_ns.h"
-#include "opt_ns_uts.h"
-#endif
 
 #if defined(NAMESPACES) && defined(NS_UTS)
 #include <sys/uts.h>
